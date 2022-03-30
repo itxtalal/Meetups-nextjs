f588c452b304ba9b15a2eb7d55292863cf497b1d
Talal <itxtalal@gmail.com>
2022-03-30 13:29:54 +0500

    Details Page

---
+++ pages/[meetupID]/index.js
@@ -0,0 +1,16 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <>
+      <MeetupDetail
+        image="https://upload.wikimedia.org/wikipedia/commons/e/e5/Cityoflondontowerbridge.jpg"
+        title="First Meetup"
+        address="London"
+        description="This is first meetup description"
+      />
+    </>
+  );
+};
+
+export default MeetupDetails;
