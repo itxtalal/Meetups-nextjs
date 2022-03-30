e496c17dcedb11f0a205ea06911780e2a1e290d9
Talal <itxtalal@gmail.com>
2022-03-30 13:36:04 +0500

    Pre-rendering Problem with fetch

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 // domain.com
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -29,7 +30,18 @@ const DUMMY_MEETUPS = [
 ];
 
 const HomePage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    // send a fetch request and load data
+    // !! Problem
+    // !! Doing this will first create HTML documnet with empty dataset in the first execution useState is empty array
+    // !! When the data is loaded in second execution, the data is updated in State, but the HTML document still has no data in it.
+    // !! HTML Doc does not contain our data in it. Not SEO friendly.
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
 };
 
 export default HomePage;
